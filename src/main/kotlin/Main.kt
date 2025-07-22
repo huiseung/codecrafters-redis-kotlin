ca4cdcd24873e9e342085f3a2e2c5692f0ae66b4
huiseung <whtjddn75@gmail.com>
2025-07-22 21:33:16 +0900

    [feature] Response to PING

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -5,12 +5,18 @@ fun main(args: Array<String>) {
     System.err.println("Logs from your program will appear here!")
 
     // Uncomment this block to pass the first stage
-    var serverSocket = ServerSocket(6379)
+    val serverSocket = ServerSocket(6379)
     //
     // // Since the tester restarts your program quite often, setting SO_REUSEADDR
     // // ensures that we don't run into 'Address already in use' errors
-    serverSocket.reuseAddress = true
+    serverSocket.use {
+        serverSocket.reuseAddress = true
+        val clientSocket = serverSocket.accept()
+        clientSocket.use {
+            val output = clientSocket.getOutputStream()
+            output.write("+PONG\r\n".toByteArray())
+            output.flush()
+        }
+    }
 
-    serverSocket.accept() // Wait for connection from client.
-    println("accepted new connection")
 }
