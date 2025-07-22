d7b6d1f1256654c756c881dca669fb8a6db1c32c
huiseung <whtjddn75@gmail.com>
2025-07-22 22:07:55 +0900

    [feature] Response to Multiple PING

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,3 +1,5 @@
+import java.io.BufferedReader
+import java.io.InputStreamReader
 import java.net.ServerSocket
 
 fun main(args: Array<String>) {
@@ -12,11 +14,20 @@ fun main(args: Array<String>) {
     serverSocket.use {
         serverSocket.reuseAddress = true
         val clientSocket = serverSocket.accept()
-        clientSocket.use {
+        clientSocket.use{
+            val input = clientSocket.getInputStream()
             val output = clientSocket.getOutputStream()
-            output.write("+PONG\r\n".toByteArray())
-            output.flush()
+            val reader = BufferedReader(InputStreamReader(input))
+
+            while(true){
+                val request = reader.readLine() ?: break
+                if(request.equals("PING", ignoreCase = true)){
+                    output.write("+PONG\r\n".toByteArray())
+                    output.flush()
+                }
+            }
         }
+
     }
 
 }
