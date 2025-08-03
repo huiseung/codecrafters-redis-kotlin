778b6f34e6084489c2b407015f3f488bdf744702
huiseung <whtjddn75@gmail.com>
2025-08-03 16:03:35 +0900

    [refactor] rdb file parsing

--- src/main/kotlin/RdbManager.kt
+++ src/main/kotlin/RdbManager.kt
@@ -1,102 +1,26 @@
+import util.parseExpiryTime
+import util.parseKey
+import util.parseStringValue
 import java.io.File
 import java.io.FileInputStream
-import java.lang.IllegalArgumentException
 
 class RdbManager(
     private val redisConfig: RedisConfig,
     private val storageService: StorageService,
 ) {
     fun loadFromDisk() {
-        if (redisConfig.get("dir") == null || redisConfig.get("dbfilename") == null) {
-            return
-        }
-        if (!File(redisConfig.get("dir"), redisConfig.get("dbfilename")).exists()) {
-            return
-        }
-        FileInputStream(redisConfig.get("dir") + "/" + redisConfig.get("dbfilename")).use { fis ->
+        val dir = redisConfig.get("dir") ?: return
+        val dbfilename = redisConfig.get("dbfilename") ?: return
+        val file = File(dir, dbfilename)
+        if (!file.exists()) return
+
+        FileInputStream(file).use { fis ->
             readHeaderSection(fis)
             readMetadataSection(fis)
             readDataBaseSection(fis)
             readEndOfFile(fis)
         }
     }
-
-    private fun parsing00(fis: FileInputStream, firstByte: Int): ByteArray {
-        val length = firstByte and 0b00111111
-        return fis.readNBytes(length)
-    }
-
-    private fun parsing01(fis: FileInputStream, firstByte: Int): ByteArray {
-        val high6 = firstByte and 0b00111111
-        val low8 = fis.read() and 0xFF
-        val length = (high6 shl 8) or low8
-        return fis.readNBytes(length)
-    }
-
-    private fun parsing10(fis: FileInputStream, firstByte: Int): ByteArray {
-        val b1 = fis.read()
-        val b2 = fis.read()
-        val b3 = fis.read()
-        val b4 = fis.read()
-        val length = ((b1 and 0xFF) shl 24) or
-                ((b2 and 0xFF) shl 16) or
-                ((b3 and 0xFF) shl 8) or
-                (b4 and 0xFF)
-        return fis.readNBytes(length)
-    }
-
-    private fun parsing11(fis: FileInputStream, firstByte: Int): Int {
-        return when (firstByte) {
-            0xC0 -> {
-                fis.read()
-            }
-
-            0xC1 -> {
-                val b1 = fis.read()
-                val b2 = fis.read()
-                ((b2 and 0xFF) shl 8) or (b1 and 0xFF)
-            }
-
-            0xC2 -> {
-                val b1 = fis.read()
-                val b2 = fis.read()
-                val b3 = fis.read()
-                val b4 = fis.read()
-                ((b4 and 0xFF) shl 24) or ((b3 and 0xFF) shl 16) or ((b2 and 0xFF) shl 8) or (b1 and 0xFF)
-            }
-
-            else -> throw IllegalArgumentException()
-        }
-    }
-
-    private fun parseKey(fis: FileInputStream): String {
-        var firstByte = fis.read()
-        var top2Bits = (firstByte shr 6) and 0b11
-        var key = ""
-        if (top2Bits == 0b11) {
-            val int = parsing11(fis, firstByte)
-            key = int.toString()
-        } else {
-            val bytes = when (top2Bits) {
-                0b00 -> {
-                    parsing00(fis, firstByte)
-                }
-
-                0b01 -> {
-                    parsing01(fis, firstByte)
-                }
-
-                0b10 -> {
-                    parsing10(fis, firstByte)
-                }
-
-                else -> throw IllegalArgumentException()
-            }
-            key = String(bytes, Charsets.UTF_8)
-        }
-        return key
-    }
-
 
     private fun readHeaderSection(fis: FileInputStream) {
         val header = fis.readNBytes(9)
@@ -133,108 +57,15 @@ class RdbManager(
         repeat(numOfNonExpiryKey) {
             val valueEncodeType = fis.read()
             var key: String = parseKey(fis)
-            //
-            var firstByte = fis.read()
-            var top2Bits = (firstByte shr 6) and 0b11
-            var value = ""
-            if (top2Bits == 0b11) {
-                val int = parsing11(fis, firstByte)
-                if (valueEncodeType == 0) {
-                    value = int.toString()
-                }
-            } else {
-                val bytes = when (top2Bits) {
-                    0b00 -> {
-                        parsing00(fis, firstByte)
-                    }
-
-                    0b01 -> {
-                        parsing01(fis, firstByte)
-                    }
-
-                    0b10 -> {
-                        parsing10(fis, firstByte)
-                    }
-
-                    else -> throw IllegalArgumentException()
-                }
-                if (valueEncodeType == 0) {
-                    value = String(bytes, Charsets.UTF_8)
-                }
-            }
-
+            var value = parseStringValue(fis)
             storageService.setString(key, value, null)
         }
         repeat(numOfExpiryKey)
         {
-
-            val timeType = fis.read()
-            var timeMs = 0L
-            if (timeType == 0xFC) {
-                val b1 = fis.read()
-                val b2 = fis.read()
-                val b3 = fis.read()
-                val b4 = fis.read()
-                val b5 = fis.read()
-                val b6 = fis.read()
-                val b7 = fis.read()
-                val b8 = fis.read()
-
-                timeMs = ((b8.toLong() and 0xFF) shl 56) or
-                        ((b7.toLong() and 0xFF) shl 48) or
-                        ((b6.toLong() and 0xFF) shl 40) or
-                        ((b5.toLong() and 0xFF) shl 32) or
-                        ((b4.toLong() and 0xFF) shl 24) or
-                        ((b3.toLong() and 0xFF) shl 16) or
-                        ((b2.toLong() and 0xFF) shl 8) or
-                        (b1.toLong() and 0xFF)
-
-            } else if (timeType == 0xFD) {
-                val b1 = fis.read()
-                val b2 = fis.read()
-                val b3 = fis.read()
-                val b4 = fis.read()
-
-                val timeSec = ((b4 and 0xFF) shl 24) or
-                        ((b3 and 0xFF) shl 16) or
-                        ((b2 and 0xFF) shl 8) or
-                        (b1 and 0xFF)
-                timeMs = timeSec.toLong() * 1000
-            }
-
-            //
+            var timeMs = parseExpiryTime(fis)
             val valueEncodeType = fis.read()
             var key: String = parseKey(fis)
-            //
-            var firstByte = fis.read()
-            var top2Bits = (firstByte shr 6) and 0b11
-            var value = ""
-            if (top2Bits == 0b11) {
-                val int = parsing11(fis, firstByte)
-                if (valueEncodeType == 0) {
-                    value = int.toString()
-                }
-            } else {
-                val bytes = when (top2Bits) {
-                    0b00 -> {
-                        parsing00(fis, firstByte)
-                    }
-
-                    0b01 -> {
-                        parsing01(fis, firstByte)
-                    }
-
-                    0b10 -> {
-                        parsing10(fis, firstByte)
-                    }
-
-                    else -> throw IllegalArgumentException()
-                }
-                if (valueEncodeType == 0) {
-                    value = String(bytes, Charsets.UTF_8)
-                }
-            }
-            //
+            val value = parseStringValue(fis)
             storageService.setString(key, value, timeMs)
         }
 
---
+++ src/main/kotlin/util/RdbFormat.kt
@@ -0,0 +1,100 @@
+package util
+
+import java.io.FileInputStream
+import java.lang.IllegalArgumentException
+
+fun parseKey(fis: FileInputStream): String {
+    val firstByte = fis.read()
+    val top2Bits = (firstByte shr 6) and 0b11
+    return when (top2Bits) {
+        0b11 -> parsing11(fis, firstByte).toString()
+        else -> String(getByteArray(fis, top2Bits, firstByte), Charsets.UTF_8)
+    }
+}
+
+fun parseStringValue(fis: FileInputStream): String {
+    val firstByte = fis.read()
+    val top2Bits = (firstByte shr 6) and 0b11
+    return if (top2Bits == 0b11) {
+        parsing11(fis, firstByte).toString()
+    } else {
+        String(getByteArray(fis, top2Bits, firstByte), Charsets.UTF_8)
+    }
+}
+
+fun parseExpiryTime(fis: FileInputStream): Long {
+    val timeType = fis.read()
+    return when (timeType) {
+        0xFC -> readLongByLittle(fis)
+        0xFD -> readIntByLittle(fis).toLong() * 1000
+        else -> throw IllegalArgumentException()
+    }
+}
+
+
+private fun getByteArray(fis: FileInputStream, top2Bits: Int, firstByte: Int): ByteArray {
+    return when (top2Bits) {
+        0b00 -> parsing00(fis, firstByte)
+        0b01 -> parsing01(fis, firstByte)
+        0b10 -> parsing10(fis, firstByte)
+        else -> throw IllegalArgumentException()
+    }
+}
+
+private fun parsing00(fis: FileInputStream, firstByte: Int): ByteArray {
+    val length = firstByte and 0b00111111
+    return fis.readNBytes(length)
+}
+
+private fun parsing01(fis: FileInputStream, firstByte: Int): ByteArray {
+    val high6 = firstByte and 0b00111111
+    val low8 = fis.read() and 0xFF
+    val length = (high6 shl 8) or low8
+    return fis.readNBytes(length)
+}
+
+private fun parsing10(fis: FileInputStream, firstByte: Int): ByteArray {
+    val b1 = fis.read()
+    val b2 = fis.read()
+    val b3 = fis.read()
+    val b4 = fis.read()
+    val length = ((b1 and 0xFF) shl 24) or
+            ((b2 and 0xFF) shl 16) or
+            ((b3 and 0xFF) shl 8) or
+            (b4 and 0xFF)
+    return fis.readNBytes(length)
+}
+
+private fun parsing11(fis: FileInputStream, firstByte: Int): Int {
+    return when (firstByte) {
+        0xC0 -> fis.read()
+        0xC1 -> readShortByteLittle(fis)
+        0xC2 -> readIntByLittle(fis)
+        else -> throw IllegalArgumentException()
+    }
+}
+
+private fun readLongByLittle(fis: FileInputStream): Long {
+    var timeMs = 0L
+    for (i in 1..8) {
+        timeMs = timeMs or (fis.read().toLong() and 0xFF shl (8 * (i - 1)))
+    }
+    return timeMs
+}
+
+private fun readIntByLittle(fis: FileInputStream): Int {
+    val b1 = fis.read()
+    val b2 = fis.read()
+    val b3 = fis.read()
+    val b4 = fis.read()
+    return ((b4 and 0xFF) shl 24) or
+            ((b3 and 0xFF) shl 16) or
+            ((b2 and 0xFF) shl 8) or
+            (b1 and 0xFF)
+}
+
+private fun readShortByteLittle(fis: FileInputStream): Int {
+    val b1 = fis.read()
+    val b2 = fis.read()
+    return ((b2 and 0xFF) shl 8) or (b1 and 0xFF)
+}
